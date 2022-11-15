0f11e1b9698de9682a71e302899bfbcbd499cc71
Kacper Grabiec <grabiec.kacper01@gmail.com>
2022-11-15 21:28:21 +0100

    circle drawing works!

--- editor.js
+++ editor.js
@@ -36,6 +36,7 @@ function subStart(e) {
             break;
 
         case "CIRCLE":
+            touches.push(pos);
             break;
 
         case "FREE":
@@ -70,6 +71,15 @@ function subMove(e) {
             break;
 
         case "CIRCLE":
+            if (touches.length > 1) {
+                touches.pop();
+                touches.push(pos);
+
+            } else {
+                touches.push(pos);
+            }
+
+            drawCircle();
             break;
 
         case "FREE":
@@ -93,6 +103,7 @@ function subEnd(e) {
             break;
 
         case "CIRCLE":
+            subCtx.closePath();
             break;
 
         case "FREE":
@@ -105,6 +116,7 @@ function subEnd(e) {
 
     subHistory = {
         touches: touches, 
+        type: mode,
         color: colorPicker.value, 
         weight: slidebar.value
     };
@@ -128,10 +140,32 @@ function redrawToMainCanvas() {
     mainCtx.beginPath();
     mainCtx.strokeStyle = subHistory.color;
     mainCtx.lineWidth = subHistory.weight;
-    mainCtx.moveTo(subHistory.touches[0].x, subHistory.touches[0].y);
 
-    for (let i = 1; i < subHistory.touches.length; i++) {
-        mainCtx.lineTo(subHistory.touches[i].x, subHistory.touches[i].y);
+    switch (mode) {
+        case "LINE":
+            mainCtx.moveTo(subHistory.touches[0].x, subHistory.touches[0].y);
+            mainCtx.lineTo(subHistory.touches[1].x, subHistory.touches[1].y);
+            break;
+
+        case "CIRCLE":
+            s = {
+                x: (subHistory.touches[0].x + subHistory.touches[1].x) / 2,
+                y: (subHistory.touches[0].y + subHistory.touches[1].y) / 2,
+                r: Math.sqrt(Math.pow(subHistory.touches[1].x - subHistory.touches[0].x, 2) + Math.pow(subHistory.touches[1].y - subHistory.touches[0].y, 2)) / 2
+            };
+
+            mainCtx.arc(s.x, s.y, s.r, 0, 2 * Math.PI);
+            break;
+
+        case "FREE":
+            mainCtx.moveTo(subHistory.touches[0].x, subHistory.touches[0].y);
+            for (let i = 1; i < subHistory.touches.length; i++) {
+                mainCtx.lineTo(subHistory.touches[i].x, subHistory.touches[i].y);
+            }
+            break;
+
+        default:
+            break;
     }
 
     mainCtx.stroke();
@@ -152,13 +186,35 @@ function reDrawMainCanvas() {
         mainCtx.strokeStyle = mainHistory[i].color;
         mainCtx.lineWidth = mainHistory[i].weight;
 
-        for (let j = 0; j < mainHistory[i].touches.length; j++) {
-            if (j == 0) {
-                mainCtx.moveTo(mainHistory[i].touches[j].x, mainHistory[i].touches[j].y);
+        switch (mainHistory[i].type) {
+            case "LINE":
+                mainCtx.moveTo(mainHistory[i].touches[0].x, mainHistory[i].touches[0].y);
+                mainCtx.lineTo(mainHistory[i].touches[1].x, mainHistory[i].touches[1].y);
+                break;
 
-            } else {
-                mainCtx.lineTo(mainHistory[i].touches[j].x, mainHistory[i].touches[j].y);
-            }
+            case "CIRCLE":
+                s = {
+                    x: (mainHistory[i].touches[0].x + mainHistory[i].touches[1].x) / 2,
+                    y: (mainHistory[i].touches[0].y + mainHistory[i].touches[1].y) / 2,
+                    r: Math.sqrt(Math.pow(mainHistory[i].touches[1].x - mainHistory[i].touches[0].x, 2) + Math.pow(mainHistory[i].touches[1].y - mainHistory[i].touches[0].y, 2)) / 2
+                };
+
+                mainCtx.arc(s.x, s.y, s.r, 0, 2 * Math.PI);
+                break;
+
+            case "FREE":
+                for (let j = 0; j < mainHistory[i].touches.length; j++) {
+                    if (j == 0) {
+                        mainCtx.moveTo(mainHistory[i].touches[j].x, mainHistory[i].touches[j].y);
+        
+                    } else {
+                        mainCtx.lineTo(mainHistory[i].touches[j].x, mainHistory[i].touches[j].y);
+                    }
+                }
+                break;
+
+            default:
+                break;
         }
 
         mainCtx.stroke();
@@ -173,6 +229,22 @@ function drawLine() {
     subCtx.strokeStyle = colorPicker.value;
     subCtx.moveTo(touches[0].x, touches[0].y);
     subCtx.lineTo(touches[1].x, touches[1].y);
+    subCtx.stroke();
+    subCtx.closePath();
+}
+
+function drawCircle() {
+    s = {
+        x: (touches[0].x + touches[1].x) / 2,
+        y: (touches[0].y + touches[1].y) / 2,
+        r: Math.sqrt(Math.pow(touches[1].x - touches[0].x, 2) + Math.pow(touches[1].y - touches[0].y, 2)) / 2
+    };
+
+    subCtx.clearRect(0, 0, subCanvas.width, subCanvas.height);
+    subCtx.beginPath();
+    subCtx.lineWidth = slidebar.value;
+    subCtx.strokeStyle = colorPicker.value;
+    subCtx.arc(s.x, s.y, s.r, 0, 2 * Math.PI);
     subCtx.stroke();
     subCtx.closePath();
 }
