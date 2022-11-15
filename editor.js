ec9b112c5a7871a75ea4c2b69cd000df27785703
Kacper Grabiec <grabiec.kacper01@gmail.com>
2022-11-15 14:02:04 +0100

    clear button, free drawing works & rewrite code

--- editor.js
+++ editor.js
@@ -1,24 +1,33 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-ctx.canvas.width = canvas.clientWidth;
-ctx.canvas.height = canvas.clientHeight;
+const mainCanvas = document.getElementById("main-canvas");
+const mainCtx = mainCanvas.getContext("2d");
+mainCtx.canvas.width = mainCanvas.clientWidth;
+mainCtx.canvas.height = mainCanvas.clientHeight;
 
-const btnErase = document.getElementById("btn-erase");
+const subCanvas = document.getElementById("sub-canvas");
+subCanvas.style.top = 0;
+
+const subCtx = subCanvas.getContext("2d");
+subCtx.canvas.width = subCanvas.clientWidth;
+subCtx.canvas.height = subCanvas.clientHeight;
+
+const eraseButton = document.getElementById("btn-erase");
 const slidebar = document.getElementById("slidebar");
 const colorPicker = document.getElementById("color-picker");
-const btnLine = document.getElementById("btn-line");
-const btnCircle = document.getElementById("btn-circle");
-const btnFree = document.getElementById("btn-wave");
+const lineButton = document.getElementById("btn-line");
+const circleButton = document.getElementById("btn-circle");
+const freeButton = document.getElementById("btn-free");
 
 let mode = "";
+
+let mainHistory = [];
+let subHistory = {};
 let touches = [];
 
 
-function start(e) {
+function subStart(e) {
     e.preventDefault();
 
     const touch = e.touches[0];
-    touches.push(touch);
 
     switch (mode) {
         case "LINE":
@@ -30,10 +39,10 @@ function start(e) {
         case "FREE":
             const pos = calculatePosition(touch);
 
-            ctx.beginPath();
-            ctx.lineWidth = slidebar.value;
-            ctx.strokeStyle = colorPicker.value;
-            ctx.moveTo(pos.x, pos.y);
+            subCtx.beginPath();
+            subCtx.lineWidth = slidebar.value;
+            subCtx.strokeStyle = colorPicker.value;
+            subCtx.moveTo(pos.x, pos.y);
             break;
 
         default:
@@ -41,11 +50,10 @@ function start(e) {
     }
 }
 
-function move(e) {
+function subMove(e) {
     e.preventDefault();
 
     const touch = e.touches[0];
-    touches.push(touch);
 
     switch (mode) {
         case "LINE":
@@ -56,9 +64,10 @@ function move(e) {
 
         case "FREE":
             const pos = calculatePosition(touch);
+            touches.push(pos);
 
-            ctx.lineTo(pos.x, pos.y);
-            ctx.stroke();
+            subCtx.lineTo(pos.x, pos.y);
+            subCtx.stroke();
             break;
 
         default:
@@ -66,72 +75,98 @@ function move(e) {
     }
 }
 
-function end(e) {
+function subEnd(e) {
     e.preventDefault();
-
-    const startPos = calculatePosition(touches[0]);
-    const endPos = calculatePosition(touches[touches.length - 1]);
 
     switch (mode) {
         case "LINE":
-            ctx.beginPath();
-            ctx.lineWidth = slidebar.value;
-            ctx.strokeStyle = colorPicker.value;
-            ctx.moveTo(startPos.x, startPos.y);
-            ctx.lineTo(endPos.x, endPos.y);
-            ctx.stroke();
-            ctx.closePath();
             break;
 
         case "CIRCLE":
-            x = (startPos.x + endPos.x) / 2;
-            y = (startPos.y + endPos.y) / 2;
-            r = Math.sqrt(Math.pow(x - startPos.x, 2) + Math.pow(y - startPos.y, 2)) / 2;
-
-            ctx.beginPath();
-            ctx.lineWidth = slidebar.value;
-            ctx.strokeStyle = colorPicker.value;
-            ctx.arc(x, y, r, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.closePath();
             break;
 
         case "FREE":
-            ctx.closePath();
             break;
 
         default:
+            subCtx.closePath();
             break;
     }
 
-    clearTouches();
+    subHistory = {
+        touches: touches, 
+        color: colorPicker.value, 
+        weight: slidebar.value
+    };
+
+    touches = [];
+
+    redrawToMainCanvas();
 }
 
 function calculatePosition(o) {
-    return {x: o.clientX - canvas.getBoundingClientRect().left, y: o.clientY - canvas.getBoundingClientRect().top};
+    return {x: o.clientX - mainCanvas.getBoundingClientRect().left, y: o.clientY - mainCanvas.getBoundingClientRect().top};
 }
 
-function clearTouches() {
-    touches = [];
+function redrawToMainCanvas() {
+    subCtx.clearRect(0, 0, subCanvas.width, subCanvas.height);
+
+    mainCtx.beginPath();
+    mainCtx.strokeStyle = subHistory.color;
+    mainCtx.lineWidth = subHistory.weight;
+    mainCtx.moveTo(subHistory.touches[0].x, subHistory.touches[0].y);
+
+    for (let i = 1; i < subHistory.touches.length; i++) {
+        mainCtx.lineTo(subHistory.touches[i].x, subHistory.touches[i].y);
+    }
+
+    mainCtx.stroke();
+    mainCtx.closePath();
+
+    mainHistory.push(subHistory);
+}
+
+function reDrawMainCanvas() {
+    if (mainHistory.length > 0) {
+        mainHistory.pop();
+    }
+
+    mainCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+    for (let i = 0; i < mainHistory.length; i++) {
+        mainCtx.beginPath();
+        mainCtx.strokeStyle = mainHistory[i].color;
+        mainCtx.lineWidth = mainHistory[i].weight;
+
+        for (let j = 0; j < mainHistory[i].touches.length; j++) {
+            if (j == 0) {
+                mainCtx.moveTo(mainHistory[i].touches[j].x, mainHistory[i].touches[j].y);
+
+            } else {
+                mainCtx.lineTo(mainHistory[i].touches[j].x, mainHistory[i].touches[j].y);
+            }
+        }
+
+        mainCtx.stroke();
+        mainCtx.closePath();
+    }
 }
 
 
-btnErase.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
+eraseButton.addEventListener("click", reDrawMainCanvas);
 
-btnLine.addEventListener("click", () => {
+lineButton.addEventListener("click", () => {
     mode = "LINE";
 });
 
-btnCircle.addEventListener("click", () => {
+circleButton.addEventListener("click", () => {
     mode = "CIRCLE";
 });
 
-btnFree.addEventListener("click", () => {
+freeButton.addEventListener("click", () => {
     mode = "FREE";
 });
 
-canvas.addEventListener("touchstart", start);
-canvas.addEventListener("touchmove", move);
-canvas.addEventListener("touchend", end);
+subCanvas.addEventListener("touchstart", subStart);
+subCanvas.addEventListener("touchmove", subMove);
+subCanvas.addEventListener("touchend", subEnd);
